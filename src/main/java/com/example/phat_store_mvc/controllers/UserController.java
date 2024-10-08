bd178c083887458e9db74c68c62ce856bc3da7ea
Dima8998 <dima-kirichenko8@bk.ru>
2024-10-08 20:54:18 +0500

    commit

--- src/main/java/com/example/phat_store_mvc/controllers/UserController.java
+++ src/main/java/com/example/phat_store_mvc/controllers/UserController.java
@@ -1,20 +1,12 @@
 package com.example.phat_store_mvc.controllers;
 
-import com.example.phat_store_mvc.dao.services.CategoryService;
-import com.example.phat_store_mvc.model.entities.stock.dto.CategoryDTO;
 import com.example.phat_store_mvc.model.security.ApplicationUser;
 import com.example.phat_store_mvc.repositories.ApplicationUserRepo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
